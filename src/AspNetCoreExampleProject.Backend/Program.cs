56232846aed22cc68946ae0b2583c30ec4ae50de
MaxStreicher <max.streicher@rosslight.de>
2026-04-18 18:22:44 +0200

    fix: Build warning

--- src/AspNetCoreExampleProject.Backend/Program.cs
+++ src/AspNetCoreExampleProject.Backend/Program.cs
@@ -47,7 +47,7 @@ app.MapGet(
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
